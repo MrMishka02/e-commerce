c867831c17af6a2aa71cdae3ae772be555c7067c
Mikheil Gogia <m.gogia@hotmail.com>
2023-12-29 23:23:55 +0400

    docs: added components and some configs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,8 +7,8 @@ const config = {
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
-	],
-  prefix: "tailwind.config.ts",
+  ],
+  prefix: "",
   theme: {
     container: {
       center: true,
